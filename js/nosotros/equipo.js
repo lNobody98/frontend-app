653ebac75c6e68f9c790b1fec260bf602c9f5662
purpleman <elhombremorado93@gmail.com>
2026-06-06 18:50:48 -0500

    queda

--- js/nosotros/equipo.js
+++ js/nosotros/equipo.js
@@ -194,7 +194,7 @@ const equipo = [
         },
         habilidades: ["Trabajo en equipo", "Resolución de problemas", "Comunicación efectiva"],
         certs: ["Cisco Networking Basics", "Fundamentos de Git y GitHub", "Introducción a Linux"],
-        pdf: "pdf/nosotros/cv-juan.pdf",
+        pdf: "pdf/nosotros/cv-Juan-Morales.pdf",
         iniciales: "JM",
         fotoClase: "nos-foto-juan"
     }),
